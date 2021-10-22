52dc7064ae77f61eee6e09e890bc69eb16b9b2d4
z0xyz <solicitmind@gmail.com>
2021-10-22 13:09:12 +0200

    initial commit

---
+++ file.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         forvo
+// @version      0.2
+// @description  Auto pronounce as soon as the forvo page loads .
+// @author       z0xyz
+// @match        https://*forvo.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @run-at       document-end
+// ==/UserScript==
+
+function play_first_sound(first_number,second_number){
+    document.getElementsByClassName("word-play-list-icon-size-l").item(first_number).getElementsByClassName("play").item(second_number).click()
+}
+
+function play_additional_sound(first_number,second_number){
+    document.getElementsByClassName("word-play-list-icon-size-l").item(first_number).childNodes.item(second_number).firstElementChild.click()
+}
+
+try {
+    try {
+        play_first_sound(0,0)
+    }
+    catch {
+        play_additional_sound(1,1)
+    }
+}catch {
+    document.getElementById("navLangItem-en").firstElementChild.click()
+}
+
+function keypressCheck(keypressEvent){
+    keypressEvent.preventDefault()
+    switch (keypressEvent.code) {
+        case ("Space"):
+            play_first_sound(0,0)
+            break
+        case ("Digit1"):
+            play_additional_sound(1,1)
+            break
+        case ("Digit2"):
+            play_additional_sound(1,3)
+            break
+        case ("Digit3"):
+            play_additional_sound(1,5)
+            break
+        case ("Digit4"):
+            play_additional_sound(1,7)
+            break
+    }
+}
+
+window.addEventListener("keypress",keypressCheck,false)
