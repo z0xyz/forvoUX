0eb2b1878edfd4bb520a7cf6f6e343a078136eb2
z0xyz <solicitmind@gmail.com>
2021-10-27 03:43:43 +0200

    Bug fix that made the code more flexible in case there was no matches , and therefor the addressable DOM three differed

--- file.js
+++ file.js
@@ -10,12 +10,20 @@
 // @namespace https://greasyfork.org/users/813029
 // ==/UserScript==
 
-function play_initial_sound(first_number,second_number){
-    document.getElementsByClassName("word-play-list-icon-size-l").item(first_number).getElementsByClassName("play").item(second_number).click()
+function play_initial_sound(){
+    document.getElementsByClassName("word-play-list-icon-size-l").item(0).getElementsByTagName("li").item(0).getElementsByClassName("play").item(0).click()
 }
 
-function play_additional_sound(first_number,second_number){
-    document.getElementsByClassName("word-play-list-icon-size-l").item(first_number).childNodes.item(second_number).firstElementChild.click()
+function play_additional_sound(recordingNumber){
+    let exactWordPassage = document.getElementsByClassName("results_match").item(0)
+    function playSound(recordingNumber,categoryNumber){
+        document.getElementsByClassName("word-play-list-icon-size-l").item(categoryNumber).getElementsByTagName("li").item(recordingNumber).getElementsByClassName("play").item(0).click()
+    }
+    if (exactWordPassage == null){
+        playSound(recordingNumber,0)
+    }else {
+        playSound(recordingNumber,1)
+    }
 }
 
 try {
@@ -34,19 +42,19 @@ function keypressCheck(keypressEvent){
         keypressEvent.returnValue = false;
         switch (keypressEvent.code) {
             case ("Space"):
-                play_initial_sound(0,0)
+                play_initial_sound()
                 break
             case ("Digit1"):
-                play_additional_sound(1,1)
+                play_additional_sound(0)
                 break
             case ("Digit2"):
-                play_additional_sound(1,3)
+                play_additional_sound(1)
                 break
             case ("Digit3"):
-                play_additional_sound(1,5)
+                play_additional_sound(2)
                 break
             case ("Digit4"):
-                play_additional_sound(1,7)
+                play_additional_sound(3)
                 break
         }
         (function searchBoxFunctionality(){
